555d9d7c1d2548f5c88b6f8ee5e859059abefafa
SS <a.saidi1711@gmail.com>
2022-10-20 10:50:03 +0200

    Classe Carafe de l'atelier 05.

---
+++ Atelier05/src/Carafe.java
@@ -0,0 +1,31 @@
+
+public class Carafe  {
+	private int contenu, capacite;
+	
+	public Carafe(int contenu, int capacite) {
+		contenu = contenu;
+		capacite = capacite;
+		
+	}
+	public void Vider() {
+		contenu=0;
+	}
+	public void Remplir() {
+		contenu=capacite;
+	}
+	public void Transvaser(Carafe c1, Carafe c2) {
+		if ((c1.contenu+c2.contenu)<=c2.capacite) {
+			c2.contenu=c1.contenu;
+		}else {
+			c2.contenu=c1.contenu;
+			c1.contenu=(c2.capacite-c1.contenu);
+		}
+	}
+	public int Contenu() {
+		return contenu;
+	}
+	public int Capacite() {
+		return capacite;
+	}
+
+}
